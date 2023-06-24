1a54faa19588cfdeb0b2f00fb25c3556e86b9a36
Trenzen <103641280+itsTrenzen@users.noreply.github.com>
2023-06-24 22:58:03 +0200

    Speech to Text eingefügt

--- client.py
+++ client.py
@@ -1,4 +1,54 @@
 import socket
+import speech_recognition as sr
+import pyttsx3
+from threading import Thread
+ 
+# Initialize the recognizer
+r = sr.Recognizer()
+ 
+# Function to convert text to
+# speech
+"""
+def SpeakText(command):
+     
+    # Initialize the engine
+    engine = pyttsx3.init()
+    engine.say(command)
+    engine.runAndWait()
+"""
+     
+# Loop infinitely for user to
+# speak
+ 
+while(1):   
+     
+    try:
+         
+        # use the microphone as source for input.
+        with sr.Microphone() as source2:
+             
+            # wait for a second to let the recognizer
+            # adjust the energy threshold based on
+            # the surrounding noise level
+            r.adjust_for_ambient_noise(source2, duration=0.2)
+             
+            #listens for the user's input
+            audio2 = r.listen(source2)
+             
+            # Using google to recognize audio
+            MyText = r.recognize_google(audio2)
+            MyText = MyText.lower()
+ 
+            print(MyText)
+            #SpeakText(MyText)
+             
+    except sr.RequestError as e:
+        print("Could not request results; {0}".format(e))
+         
+    except sr.UnknownValueError:
+        print("unknown error occurred")
+
+
 
 def client_program():
     host = socket.gethostname()  # as both code is running on same pc
@@ -21,4 +71,5 @@ def client_program():
 
 
 if __name__ == '__main__':
-    client_program()
+    t1 = Thread(target = client_program)
+    t2 = Thread(target = SpeakText)
