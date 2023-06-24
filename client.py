643b1abd12cdff518620ce52eb28b3b84a5a3aba
Trenzen <103641280+itsTrenzen@users.noreply.github.com>
2023-06-24 23:28:23 +0200

    Added threading

--- client.py
+++ client.py
@@ -2,10 +2,11 @@ import socket
 import speech_recognition as sr
 import pyttsx3
 from threading import Thread
+import time
  
 # Initialize the recognizer
 r = sr.Recognizer()
- 
+MyText = ' '
 # Function to convert text to
 # speech
 """
@@ -19,34 +20,34 @@ def SpeakText(command):
      
 # Loop infinitely for user to
 # speak
- 
-while(1):   
+def record():
+    while(1):   
      
-    try:
+        try:
          
-        # use the microphone as source for input.
-        with sr.Microphone() as source2:
+            # use the microphone as source for input.
+            with sr.Microphone() as source2:
              
-            # wait for a second to let the recognizer
-            # adjust the energy threshold based on
-            # the surrounding noise level
-            r.adjust_for_ambient_noise(source2, duration=0.2)
+                # wait for a second to let the recognizer
+                # adjust the energy threshold based on
+                # the surrounding noise level
+                r.adjust_for_ambient_noise(source2, duration=0.2)
              
-            #listens for the user's input
-            audio2 = r.listen(source2)
+                #listens for the user's input
+                audio2 = r.listen(source2)
              
-            # Using google to recognize audio
-            MyText = r.recognize_google(audio2)
-            MyText = MyText.lower()
+                # Using google to recognize audio
+                MyText = r.recognize_google(audio2)
+                MyText = MyText.lower()
  
-            print(MyText)
-            #SpeakText(MyText)
+                print(MyText)
+                #SpeakText(MyText) die Stimme nervt
              
-    except sr.RequestError as e:
-        print("Could not request results; {0}".format(e))
+        except sr.RequestError as e:
+            print("Could not request results; {0}".format(e))
          
-    except sr.UnknownValueError:
-        print("unknown error occurred")
+        except sr.UnknownValueError:
+            print("unknown error occurred")
 
 
 
@@ -57,19 +58,24 @@ def client_program():
     client_socket = socket.socket()  # instantiate
     client_socket.connect((host, port))  # connect to the server
 
-    message = input(" -> ")  # take input
+    #message = input(" -> ")  # take input
+    message = MyText
 
     while message.lower().strip() != 'bye':
+        
         client_socket.send(message.encode())  # send message
         data = client_socket.recv(1024).decode()  # receive response
 
         print('Received from server: ' + data)  # show in terminal
 
-        message = input(" -> ")  # again take input
+        #message = input(" -> ")  # again take input
+        message = MyText
+        time.sleep(0.5)
 
     client_socket.close()  # close the connection
 
-
 if __name__ == '__main__':
     t1 = Thread(target = client_program)
-    t2 = Thread(target = SpeakText)
+    t2 = Thread(target = record)
+    t1.start()
+    t2.start()
--- server.py
+++ server.py
@@ -13,9 +13,10 @@ def server_program():
     server_socket.bind((host, port))  # bind host address and port together
 
     # configure how many client the server can listen simultaneously
-    server_socket.listen(2)
+    server_socket.listen(3)
     conn, address = server_socket.accept()  # accept new connection
     print("Connection from: " + str(address))
+    
     while True:
         # receive data stream. it won't accept data packet greater than 1024 bytes
         data = conn.recv(1024).decode()
