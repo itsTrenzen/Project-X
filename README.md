074510b54009e6771d4efb5ba4610415c9416949
Trenzen <103641280+itsTrenzen@users.noreply.github.com>
2023-06-29 16:06:44 +0200

    included Resemble api and first functions
    It's now possible to manage api projects

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Project X
  An upcoming revolution
+
+# The following libraries are required to run the program:
+    1. pip install resemble
+    2. pip install speechrecognition
+    3. pip install pyaudio
+    4. pip install googletrans==4.0.0-rc1
--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 from googletrans import Translator
 import socket
+from resemble import Resemble
+
+Resemble.api_key('zVGJevvgoXrs2NGTG8GKFAtt')
 
 def server_program():
     translator = Translator()
@@ -19,7 +22,7 @@ def server_program():
         if not data:
             break
         print("From connected user: " + str(data))
-
+        #Server protocol
         splitdata = data.split(";")
         if splitdata[0] == '?TRANS':
             translated_text = translator.translate(splitdata[1], dest=splitdata[2]).text
@@ -33,6 +36,28 @@ def server_program():
             conn.send(translated_text.encode())
 
     conn.close()
+
+#API functions
+def createProject(name, desc, isPublic, isArchived, isCollaborative):
+    response = Resemble.v2.projects.create(name, desc, isPublic, isCollaborative, isArchived)
+    project = response['item']
+    return project
+
+def getProject(uuid):
+    response = Resemble.v2.projects.get(uuid)
+    project = response['item']
+    return project
+
+def getAllProjects(page, pageSize):
+    respone = Resemble.v2.projects.all(page, pageSize)
+    projects = respone['items']
+    return projects
+
+def updateProject(uuid, name, desc, isPublic, isArchived, isCollaborative):
+    response = Resemble.v2.projects.update(uuid, name, desc, isPublic, isCollaborative, isArchived)
+
+def deleteProject(uuid):
+    response = Resemble.v2.projects.delete(uuid)
 
 if __name__ == '__main__':
     server_program()
